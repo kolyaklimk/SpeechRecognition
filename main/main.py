574a8b13049959ca0e22bc96224fca29a5f95c9a
Kolya <kolyaklimk@gmail.com>
2022-08-22 11:37:59 +0300

    add window.py and main.py

---
+++ main/main.py
@@ -0,0 +1,11 @@
+import sys
+from PyQt6.QtWidgets import QApplication, QMainWindow
+from window import Ui_MainWindow
+
+app = QApplication(sys.argv)
+mainWindow = QMainWindow()
+ui = Ui_MainWindow()
+ui.setupUi(mainWindow)
+
+mainWindow.show()
+sys.exit(app.exec())
